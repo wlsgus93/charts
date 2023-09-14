c362806c037785526be79b0043c8e05d2775ddbc
wlsgus93 <wlsgus63@naver.com>
2023-09-15 02:22:24 +0900

    feat: 그래프 원형 작성 및 mock_data 세팅

--- src/App.js
+++ src/App.js
@@ -1,50 +1,131 @@
-import { useRef,useEffect } from "react";
-import { Line } from "react-chartjs-2";
-import styled from "styled-components";
-function App() {
-  const chartRef = useRef(null);
-  const options = {
-    scales: {
-        x: {
-            type: 'category',
-            labels: ['January', 'February', 'March', 'April', 'May'],
-        },
-        // 다른 스케일 설정
-    },
-    // 다른 옵션 설정
-};
-  useEffect(() => {
-      // 이전 차트 제거
-      if (chartRef.current) {
-          const chartInstance = chartRef.current.chartInstance;
-          if (chartInstance) {
-              chartInstance.destroy();
-          }
-      }
-  }, []);
+import React, {useEffect, useState} from 'react';
+// import mockData from '/mock_data.json';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  Title,
+  Filler,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
+import styled from 'styled-components';
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  Title,
+  Filler,
+  
+);
 
+export const options = {
+  responsive: true,
+  interaction: {
+    mode: 'index',
+    intersect: false,
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js  Chart - Multi Axis',
+    },
+  },
+  scales: {
+    y: {
+      display: true,
+      position: 'left',
+    },
+    y1: {
+      display: true,
+      position: 'right',
+      grid: {
+        drawOnChartArea: false,
+      },
+    },
+  },
+};
+
+function App() {
+  const [datas, setData] = useState(null);
+
+  useEffect(()=>{
+
+    const jsonPath = '/mock_data.json';
+
+    // JSON 파일 가져오기
+    fetch(jsonPath)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result.response)
+        setData(result);
+      })
+      .catch((error) => {
+        console.error('Error fetching JSON:', error);
+      });
+
+  },[])
+ 
+  if (!datas) {
+    return <div>Loading...</div>;
+  }
+ 
+  const dates = Object.keys(datas.response);
+  const areaData = dates.map((date) => datas.response[date].value_area);
+  const valueDatas = dates.map((date) => datas.response[date].value_bar);
+  console.log(dates);
+  console.log(areaData);
+  console.log(valueDatas);
 
   const data = {
-    labels: ['January', 'February', 'March', 'April', 'May'],
+    labels: dates,
     datasets: [
-        {
-            label: 'Sales Data',
-            fill: false,
-            lineTension: 0.1,
-            borderColor: 'rgba(75,192,192,1)',
-            borderWidth: 2,
-            data: [65, 59, 80, 81, 56],
-        },
+      {
+        type: 'line',
+        label: 'area',
+        borderColor: 'rgb(255, 99, 132)',
+        borderWidth: 2,
+        fill: true,
+        backgroundColor: 'rgb(75, 2, 192)',
+        data: areaData,
+        yAxisID: 'y'
+      },
+      {
+        type: 'bar',
+        label: 'bar',
+        backgroundColor: 'rgb(75, 192, 192)',
+        data: valueDatas,
+        borderColor: 'white',
+        borderWidth: 2,
+        yAxisID: 'y1'
+      },
     ],
-};
+  };
+
+
   
 
 return (
   <Container>
-    <Line  ref={chartRef} type="line" data={data} options={options} />
+    <Chart options={options} data={data} />;
   </Container>
 );
 }
+
+
 
 export default App;
 const Container = styled.div`
